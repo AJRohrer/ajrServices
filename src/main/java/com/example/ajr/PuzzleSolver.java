886aa51225467166a195dda40f6554d75acd081f
AJRohrer <andrewjrohrer@gmail.com>
2019-02-25 21:13:39 -0600

    Finish core logic.

--- src/main/java/com/example/ajr/PuzzleSolver.java
+++ src/main/java/com/example/ajr/PuzzleSolver.java
@@ -18,74 +18,140 @@ final class PuzzleSolver {
 
         locations = getAllStrings(WordSearchArray);
 
+        findAndAddLocations(locations,WordSearchArray,WordToFind);
+
         return _allLocations;
     }
 
-    private static void findAndAddLocations(ArrayList<Location> locList){
+    private static ArrayList<ArrayList<Location>> findAndAddLocations(ArrayList<ArrayList<Location>> arrStrings, char[][] wsArray, String wordToFind){
+        ArrayList<ArrayList<Location>> wsBase = new ArrayList<>();
 
+        wsBase = arrStrings;
+
+        //loop through each array of positions (strings) generated from each possible direction.
+        for (int x = 0; x < arrStrings.size();x++){
+            int wordToFindCount = 0;
+            boolean fullMatchFound = false;
+
+            //loop through the positions of each string
+            for (int strLocCount = 0; strLocCount < arrStrings.get(x).size(); strLocCount++){
+                //if the current letter matches the first letter of the word to find
+                if (arrStrings.get(x).get(strLocCount).letter() == wordToFind.charAt(wordToFindCount)){
+
+                    //peek ahead without incrementing strLocCount to see if there is a full match.
+                    for (int matchCount = 1; matchCount <= wordToFind.length(); matchCount++){
+                        if (arrStrings.get(x).get(strLocCount + matchCount).letter() == wordToFind.charAt(wordToFindCount + matchCount)){
+                            fullMatchFound = true;
+                        } else {
+                            fullMatchFound = false;
+                        }
+                    }
+                    //if there is a full match to the word to find flip all locations to true
+                    if (fullMatchFound == true){
+                        for (int i = 0; i < wordToFind.length();i++){
+                            //flip the locations to true that are used
+                            wsBase.get(x + i).get(strLocCount + i).setIsUsedLetter(true);
+                        }
+                    }
+                    strLocCount = strLocCount + wordToFind.length();
+
+                } else {
+                    strLocCount++;
+                }
+            }
+        }
+
+        return wsBase;
     }
 
 
     private static ArrayList<ArrayList<Location>> getAllStrings(char[][] wsArray){
         ArrayList<ArrayList<Location>> allStrings = new ArrayList<>();
 
-        allStrings.add(leftToRightLocations(wsArray));
-        allStrings.add(rightToLeftLocations(wsArray));
-        allStrings.add(topToBottomLocations(wsArray));
-        allStrings.add(bottomToTopLocations(wsArray));
-        allStrings.add(upToRightLocations(wsArray));
-        allStrings.add(upToLeftLocations(wsArray));
-        allStrings.add(downToLeftLocations(wsArray));
-        allStrings.add(downToRightLocations(wsArray));
+
+        for(ArrayList<Location> ltrarr: leftToRightLocations(wsArray)){ allStrings.add(ltrarr); }
+
+        for (ArrayList<Location> rtlarr: rightToLeftLocations(wsArray)){ allStrings.add(rtlarr); }
+
+        for (ArrayList<Location> ttbarr: topToBottomLocations(wsArray)){ allStrings.add(ttbarr); }
+
+        for (ArrayList<Location> bttarr: bottomToTopLocations(wsArray)) { allStrings.add(bttarr); }
+
+        for (ArrayList<Location> utrarr: upToRightLocations(wsArray)) { allStrings.add(utrarr); }
+
+        for (ArrayList<Location> utlarr: upToLeftLocations(wsArray)) { allStrings.add(utlarr); }
+
+        for (ArrayList<Location> dtlarr: downToLeftLocations(wsArray)) { allStrings.add(dtlarr); }
+
+        for (ArrayList<Location> dtrarr: downToRightLocations(wsArray)) { allStrings.add(dtrarr); }
 
         return allStrings;
     }
 
-    private static ArrayList<Location> leftToRightLocations(char[][] wsArray){
+    private static ArrayList<Location> addLineToAggregateList(ArrayList<Location> lineLocations){
+        ArrayList<Location> templist = new ArrayList<>();
+        for (Location l: lineLocations){ templist.add(l); }
+        return templist;
+    }
+
+    private static ArrayList<ArrayList<Location>> leftToRightLocations(char[][] wsArray){
+        ArrayList<ArrayList<Location>> allLeftToRight = new ArrayList<>();
         ArrayList<Location> leftToRight = new ArrayList<Location>();
 
         for (int x = 0; x < wsArray.length; x++){
             for (int z = 0; z < wsArray[0].length; z++){
                 leftToRight.add(new Location(false,x,z,wsArray[x][z]));
             }
+            allLeftToRight.add(addLineToAggregateList(leftToRight));
+            leftToRight.clear();
         }
-        return leftToRight;
+        return allLeftToRight;
     }
 
-    private static ArrayList<Location> rightToLeftLocations(char[][] wsArray){
+    private static ArrayList<ArrayList<Location>> rightToLeftLocations(char[][] wsArray){
+        ArrayList<ArrayList<Location>> allRightToLeft = new ArrayList<>();
         ArrayList<Location> rightToLeft = new ArrayList<Location>();
 
         for (int x = 0; x < wsArray.length; x++){
             for (int z = wsArray[0].length -1; z >= 0; z--){
                 rightToLeft.add(new Location(false,x,z, wsArray[x][z]));
             }
+            allRightToLeft.add(addLineToAggregateList(rightToLeft));
+            rightToLeft.clear();
         }
-        return rightToLeft;
+        return allRightToLeft;
     }
 
-    private static ArrayList<Location> topToBottomLocations(char[][]wsArray){
+    private static ArrayList<ArrayList<Location>> topToBottomLocations(char[][]wsArray){
+        ArrayList<ArrayList<Location>> allTopToBottom = new ArrayList<>();
         ArrayList<Location> topToBottom = new ArrayList<Location>();
 
         for (int x = 0; x < wsArray[0].length; x++){
             for(int z = 0; z < wsArray.length; z++){
                 topToBottom.add(new Location(false,z,x,wsArray[z][x]));
             }
+            allTopToBottom.add(addLineToAggregateList(topToBottom));
+            topToBottom.clear();
         }
-        return topToBottom;
+        return allTopToBottom;
     }
 
-    private static ArrayList<Location> bottomToTopLocations(char[][]wsArray) {
+    private static ArrayList<ArrayList<Location>> bottomToTopLocations(char[][]wsArray) {
+        ArrayList<ArrayList<Location>> allBottomToLeft = new ArrayList<>();
         ArrayList<Location> bottomToTop = new ArrayList<Location>();
 
         for (int x = wsArray[0].length -1; x >= 0; x--){
             for (int z = wsArray.length -1; z >= 0; z--){
                 bottomToTop.add(new Location(false,z,x,wsArray[z][x]));
             }
+            allBottomToLeft.add(addLineToAggregateList(bottomToTop));
+            bottomToTop.clear();
         }
-        return bottomToTop;
+        return allBottomToLeft;
     }
 
-    private static ArrayList<Location> downToRightLocations(char[][] wsArray){
+    private static ArrayList<ArrayList<Location>> downToRightLocations(char[][] wsArray){
+        ArrayList<ArrayList<Location>> allDownToRight = new ArrayList<>();
         ArrayList<Location> downToRight = new ArrayList<>();
 
         for (int z = wsArray[0].length - 3; z >=0; z--){
@@ -96,7 +162,8 @@ final class PuzzleSolver {
                 z++;
                 if (z > wsArray[0].length - 1) break;
             }
-
+            allDownToRight.add(addLineToAggregateList(downToRight));
+            downToRight.clear();
             z = tempz;
         }
 
@@ -108,14 +175,16 @@ final class PuzzleSolver {
                 x++;
                 if (x > wsArray.length - 1) break;
             }
-
+            allDownToRight.add(addLineToAggregateList(downToRight));
+            downToRight.clear();
             x = tempx;
         }
 
-        return downToRight;
+        return allDownToRight;
     }
 
-    private static ArrayList<Location> downToLeftLocations(char[][] wsArray){
+    private static ArrayList<ArrayList<Location>> downToLeftLocations(char[][] wsArray){
+        ArrayList<ArrayList<Location>> allDownToLeft = new ArrayList<>();
         ArrayList<Location> downtoLeft = new ArrayList<>();
 
         for(int z  = 2; z < wsArray[0].length; z++){
@@ -125,6 +194,8 @@ final class PuzzleSolver {
                 z--;
                 if (z < 0) break;
             }
+            allDownToLeft.add(addLineToAggregateList(downtoLeft));
+            downtoLeft.clear();
             z = tempz;
         }
 
@@ -136,14 +207,16 @@ final class PuzzleSolver {
                 x++;
                 if (x > wsArray.length - 1) break;
             }
-
+            allDownToLeft.add(addLineToAggregateList(downtoLeft));
+            downtoLeft.clear();
             x = tempx;
         }
 
-        return downtoLeft;
+        return allDownToLeft;
     }
 
-    private static ArrayList<Location> upToRightLocations(char[][] wsArray){
+    private static ArrayList<ArrayList<Location>> upToRightLocations(char[][] wsArray){
+        ArrayList<ArrayList<Location>> allUpToRight = new ArrayList<>();
         ArrayList<Location> upToRight = new ArrayList<>();
 
         for (int x = wsArray.length-1; x >= 2; x--){
@@ -154,7 +227,8 @@ final class PuzzleSolver {
                 x--;
                 if (x < 0) break;
             }
-
+            allUpToRight.add(addLineToAggregateList(upToRight));
+            upToRight.clear();
             x = tempx;
         }
 
@@ -166,14 +240,17 @@ final class PuzzleSolver {
                 z++;
                 if (z > wsArray[0].length -1) break;
             }
-
+            allUpToRight.add(addLineToAggregateList(upToRight));
+            upToRight.clear();
             z = tempz;
         }
 
-        return upToRight;
+        return allUpToRight;
     }
 
-    private static ArrayList<Location> upToLeftLocations(char[][] wsArray){
+    private static ArrayList<ArrayList<Location>> upToLeftLocations(char[][] wsArray){
+
+        ArrayList<ArrayList<Location>> allUpToLeft = new ArrayList<>();
         ArrayList<Location> upToLeft = new ArrayList<>();
 
         for (int z = 2; z < wsArray[0].length; z++){
@@ -183,6 +260,8 @@ final class PuzzleSolver {
                 z--;
                 if (z < 0) break;
             }
+            allUpToLeft.add(addLineToAggregateList(upToLeft));
+            upToLeft.clear();
             z = tempz;
         }
 
@@ -194,10 +273,11 @@ final class PuzzleSolver {
                 x--;
                 if (x < 0) break;
             }
-
+            allUpToLeft.add(addLineToAggregateList(upToLeft));
+            upToLeft.clear();
             x = tempx;
         }
-        return upToLeft;
+        return allUpToLeft;
     }
 
 
