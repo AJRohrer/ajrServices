3c8f85920f681d682cc26009a8702b37d9c76c72
Andrew Rohrer <andrewjrohrer@gmail.com>
2019-05-24 20:00:27 -0500

    Revert root for deployment

--- WordSearchV2/wordsearchscripts.js
+++ WordSearchV2/wordsearchscripts.js
@@ -1,4 +1,4 @@
-var hostURL = "http://localhost:8080/" //wsSolver-ROOT/
+var hostURL = "http://localhost:8080/wsSolver-ROOT/";
 $(document).ready(function(){
 
     $("#btnSolveWordSearch").on("click", function(){
